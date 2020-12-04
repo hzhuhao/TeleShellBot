5b256d25a4b00d167cd3f533582764b07f88272a
huhao <hzhuhao@163.com>
2020-12-04 16:55:24 +0800

    fix

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    config:function(){
+        return (
+            {
+                adminUsers:[1487637875], //admin users' telegram id, should be numbers
+                botToken: '1477338873:AAGUTNjDJl3fsVN-jV0NBpHwHPsmBdCJqMU', // bot token
+            }
+        );
+    }
+};
---
+++ index.js
@@ -0,0 +1,48 @@
+const config = require('./config').config();
+const TeleBot = require('telebot');
+const bot = new TeleBot(config.botToken);
+const { spawn } = require('child_process');
+const adminUsers = config.adminUsers;
+
+
+
+bot.on('text', (msg) => {
+    const id = msg.from.id;
+    msg.reply.text('$:'+msg.text);
+    if(adminUsers.indexOf(id) < 0){
+        msg.reply.text('You are not admin!');
+        return;
+    }
+    let words = msg.text.split(" ");
+    let len = words.length;
+    let args = [];
+    if (len > 1 ){
+        args = words.slice(1, len);
+        
+    } 
+    console.log('args:'+args);
+
+    const shell = spawn(words[0],args).on('error', function( err ){ 
+        		msg.reply.text('error while executing:'+words[0]);
+        		msg.reply.text(err);
+        	});
+    
+    if(shell){
+    
+    	shell.stdout.on('data', (data) => {
+        	msg.reply.text(`stdout:\n ${data}`);
+    	});
+    
+    	shell.stderr.on('data', (data) => {
+        	msg.reply.text(`stderr: ${data}`);
+    	});
+    
+    	shell.on('close', (code) => {
+        	msg.reply.text(`shell exited with code ${code}`);
+    	});
+    }
+
+}
+);
+
+bot.start();
